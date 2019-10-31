7a29bbf241a9ca61555cdd63dbd4d1cdc3cd0989
Giovani Cuenca <giovani.cuenca@gmail.com>
2019-10-31 18:39:40 -0300

    Updating API

--- src/pages/Info/index.js
+++ src/pages/Info/index.js
@@ -1,66 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import percentColor from '../../util/percentColor';
 import { Lista, Percentual } from './styles';
 
+import api from '../../services/api';
+
 export default function Info() {
+  const [obrigacoes, setObrigacoes] = useState([]);
+
+  useEffect(() => {
+    async function loadObrigacoes() {
+      const response = await api.get('obrigacoes');
+
+      const data = response.data.map(obrigacao => ({
+        ...obrigacao,
+      }));
+
+      setObrigacoes(data);
+    }
+
+    loadObrigacoes();
+  }, []);
+
   return (
     <Lista>
-      <li>
-        <div id="box">
-          <h3>Obrigações Geradas</h3>
-          <div id="circulocinza">
-            <div id="quadradobranco">
-              <Percentual backgroundColor={percentColor(20)} height={20}>
-                <div id="grafico" />
-              </Percentual>
+      {obrigacoes.map(obrigacao => (
+        <li key={obrigacao.id}>
+          <div id="box">
+            <h3>{obrigacao.title}</h3>
+            <div id="circulocinza">
+              <div id="quadradobranco">
+                <Percentual backgroundColor={percentColor(20)} height={20}>
+                  <div id="grafico" />
+                </Percentual>
+              </div>
             </div>
+            <strong>{obrigacao.info}</strong>
           </div>
-          <strong>25 de 116</strong>
-        </div>
-        <small>Ver Mais</small>
-      </li>
-      <li>
-        <div id="box">
-          <h3>Obrigações validadas no PVA</h3>
-          <div id="circulocinza">
-            <div id="quadradobranco">
-              <Percentual backgroundColor={percentColor(80)} height={80}>
-                <div id="grafico" />
-              </Percentual>
-            </div>
-          </div>
-          <strong>25 de 116</strong>
-        </div>
-        <small>Ver Mais</small>
-      </li>
-      <li>
-        <div id="box">
-          <h3>Relatórios e verificações</h3>
-          <div id="circulocinza">
-            <div id="quadradobranco">
-              <Percentual backgroundColor={percentColor(45)} height={45}>
-                <div id="grafico" />
-              </Percentual>
-            </div>
-          </div>
-          <strong>25 de 116</strong>
-        </div>
-        <small>Ver Mais</small>
-      </li>
-      <li>
-        <div id="box">
-          <h3>Obrigações transmitidas</h3>
-          <div id="circulocinza">
-            <div id="quadradobranco">
-              <Percentual backgroundColor={percentColor(65)} height={65}>
-                <div id="grafico" />
-              </Percentual>
-            </div>
-          </div>
-          <strong>25 de 116</strong>
-        </div>
-        <small>Ver Mais</small>
-      </li>
+          <small>Ver Mais</small>
+        </li>
+      ))}
     </Lista>
   );
 }
